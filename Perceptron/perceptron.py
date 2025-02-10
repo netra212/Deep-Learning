b9b904611464b1ab3c78eda1e04616c9b163373a
netr212 <netra200021kcbdr@gmail.com>
2025-02-10 18:42:44 +0000

    Perceptron notes

--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -7,6 +7,41 @@
 # Limitations of Perceptrons:
 - Perceptron only works on linear data or the sort of linear data. 
 - Perceptorn will fails on the completly non-linear data. 
-
-
 '''
+
+# Importing the required Libraries. 
+import numpy as np 
+import pandas as pd 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+from sklearn.linear_model import Perceptron
+from mlextend.plotting import plot_decision_boundary
+
+# Loading an dataset. 
+df = pd.read_csv("data/placement.csv")
+
+print(df.shape)
+print(df.head())
+
+# Visualizing the dataset. 
+print(sns.scatterplot(x=df['cgpa'],y=df['resume_score'],hue=df['placed']))
+print(plt.show())
+
+# Separating X and y.
+X = df.iloc[:, 0:2]
+y = df.iloc[:, -1]
+
+# Initializing the Perceptron class.
+perceptron = Perceptron()
+
+# Training the perceptron means calculating the values of `weight` and `bais`. 
+perceptron.fit(X, y) 
+
+# Displaying the coefficient value. 
+print(perceptron.coef_)
+
+# Displaying the interncept value. 
+print(perceptron.intercept_)
+
+# Plotting the decision boundary. 
+print(plot_decision_boundary(X.values, y.values, clf=perceptron, legend=2))
