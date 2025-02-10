f94a2f200812f4a203428145aabb2a20e8ad4ea2
netr212 <netra200021kcbdr@gmail.com>
2025-02-10 18:30:30 +0000

    adding perceptron notes

---
+++ Perceptron/perceptron.py
@@ -0,0 +1,12 @@
+'''
+# Perceptron: Mathematical functionor or models which works on the supervised ML. 
+- Perceptron is a line in 2D space which create a region in order to divide the dataset into two classes means, peceptron performs the binary classification. 
+- In 2D - Perceptron acts as a Line. 
+- In 3D - Perceptron acts as a Plane.
+
+# Limitations of Perceptrons:
+- Perceptron only works on linear data or the sort of linear data. 
+- Perceptorn will fails on the completly non-linear data. 
+
+
+'''
