34b35ab56f5c715eb16413f0611e4467d948cf83
netr212 <netra200021kcbdr@gmail.com>
2025-02-12 09:40:17 +0000

    Probelms with Peceptrons Models

--- Perceptron/perceptron_loss_function.py
+++ Perceptron/perceptron_loss_function.py
@@ -1,2 +1,6 @@
 '''
+# Problem with Perceptron Trick:
+- Line move when only there is missclassified point means the value of weight such as w1, w2 ..etc and bias such as b1, b2 will change but if there is no missclassified point, the value of the weight and bias will not change so that we cannot quantify the result or we cannot say how good is this perceptron model. 
+
+- 
 '''
